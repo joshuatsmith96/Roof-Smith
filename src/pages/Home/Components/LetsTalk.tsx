f1cb3df59618b03441fb7eb0f73edbab5c5adbd7
Josh Smith <jsmith@dailyprinting.local>
2024-01-17 14:47:03 -0600

    Adjusting the "Lets Talk" section

--- src/pages/Home/Components/LetsTalk.tsx
+++ src/pages/Home/Components/LetsTalk.tsx
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom'
 function LetsTalk(){
     return(
         <div className="LetsTalk">
+            <div className="Hero-Filter"></div>
+            <div className="LetsTalkContent">
             <h1>Lets talk about your roof!</h1>
             <p>Call us at 222-222-2222, or click the button below for your free quote today.</p>
             <Link to="/Contact" className='linkButton'>Contact Us</Link>
+            </div>
         </div>
     )
 }

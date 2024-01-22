48615014ad50053ba01385865261a12b4ef4e789
Josh Smith <jsmith@dailyprinting.local>
2024-01-22 09:38:45 -0600

    ghpages update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/Roof-Smith/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Roof-Smith/'
+  }
+
+  return config
 })
